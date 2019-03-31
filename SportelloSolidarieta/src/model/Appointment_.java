30a982659232cca24e9af1336c4270882549425c
federico <federico.porreca@gmail.com>
2019-03-31 15:02:31 +0200

    Fix dependencies

--- SportelloSolidarieta/src/model/Appointment_.java
+++ SportelloSolidarieta/src/model/Appointment_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-03-29T14:11:46.634+0100")
+@Generated(value="Dali", date="2019-03-31T14:48:22.120+0200")
 @StaticMetamodel(Appointment.class)
 public class Appointment_ {
 	public static volatile SingularAttribute<Appointment, Integer> idAppointment;
